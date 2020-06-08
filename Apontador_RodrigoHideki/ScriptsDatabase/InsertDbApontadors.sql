1f42721c4abab32880e8749456302174b8a86506
Rodrigo Melo <Rodrigo.Melo@processor.com.br>
2020-06-08 04:46:01 -0300

    Commit

---
+++ Apontador_RodrigoHideki/ScriptsDatabase/InsertDbApontadors.sql
@@ -0,0 +1,26 @@
+USE [ApontadorRodrigo]
+Go
+
+SET IDENTITY_INSERT [dbo].[Apontadors] ON 
+
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (1,N'FARMA VIVA - Farmácia de Manipulação No Campo Belo',  N'(11)9999-9999', N'Rua Leopoldo Couto de Magalhães Jr.,1223')
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (2,N'FARMA W - Farmácia de Manipulação No Brás',  N'(11)2354-9999', N'Rua João,1224')
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (3,N'FARMA XYZ - Farmácia de Manipulação No Brooklin',  N'(11)4156-9999', N'Rua Heitor Jr.,1225')
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (4,N'FARMA LIFE - Farmácia de Manipulação',  N'(11)2357-9999', N'Rua Maria Antonieta,1226')
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (5,N'BIOFARMA - Farmácia de Manipulação',  N'(11)9263-9999', N'Rua Flores,137')
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (6,N'NATUREFARMA - Farmácia de Manipulação',  N'(11)9589-9999', N'Avenida das Farmácias.,58')
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (7,N'DROGARIA SÃO PAULO',  N'(11)9827-9999', N'Rua Solange,99')
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (8,N'DROGASIL',  N'(11)9897-9999', N'Rua Raquel da Silva,879')
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (9,N'UNIPHARMA - Farmácia de Manipulação No Tucuruvi',  N'(11)9999-5234', N'Avenida União,1354')
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (10,N'MAXPHARMA - Farmácia de Manipulação Zona Leste',  N'(11)9999-4321', N'Rua Leonardo Jr.,1298')
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (11,N'FARMADOCTOR - Farmácia de Manipulação Jardins',  N'(11)9999-8777', N'Rua de trás,1453')
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (12,N'FARMAIS - Farmácia de Manipulação',  N'(11)9999-8789', N'Rua sem nome,587')
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (13,N'DROGA RAIA',  N'(11)9999-2342', N'Rua Nazaré,789')
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (14,N'FARMAFÓRMULA - Farmácia de Manipulação Itaquera',  N'(11)9874-9999', N'Avenida Fórmula,523')
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (15,N'FARMAREDE - Farmácia de Manipulação Patriarca',  N'(11)9561-9999', N'Rua dos números,498')
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (16,N'DROGARIA ONOFRE',  N'(11)9635-9999', N'Avenida Casino,1358')
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (17,N'ULTRAFARMA',  N'(11)9234-9999', N'Avenida Visconde,187')
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (18,N'DROGARIA ABC',  N'(11)9879-9999', N'Rua Lisboa,188')
+  INSERT [dbo].[Apontadors] ([ID], [NOME], [ENDERECO], [TELEFONE]) VALUES (19,N'FARMA VIVA - Farmácia de Manipulação No Campo Belo',  N'(11)9138-5974', N'Rua Leopoldo Couto de Magalhães Jr.,1225')
+
+ SET IDENTITY_INSERT [dbo].[Apontadors] OFF
